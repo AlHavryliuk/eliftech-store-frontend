bb7ae077496c360eaed31a989ebd32d7b1c95df6
AlekasndrHavryliuk <104818533+AlekasndrHavryliuk@users.noreply.github.com>
2023-05-26 01:18:48 +0300

    fix-package-json

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-  <BrowserRouter basename="/eliftech-store-backend">
+  <BrowserRouter basename="/eliftech-store-frontend">
     <Provider store={store}>
       <App />
     </Provider>
